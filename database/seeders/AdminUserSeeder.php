5a1d211813efd6f568e2f4bb743f9b787f37c444
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-28 09:35:11 +0430

    feat(database): add admin user seeder
    - Create AdminUserSeeder to populate initial admin user
    - Set default admin credentials with hashed password
    - Assign admin role and verify email timestamp
    - Integrate seeder within database seeding framework

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('11223344'),
+            'role' => 'admin',
+            'email_verified_at' => now(),
+        ]);
+    }
+}
